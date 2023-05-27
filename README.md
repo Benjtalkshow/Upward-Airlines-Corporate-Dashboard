689113ab74e2e68ede966514083f0bc4bda03753
Benjtalkshow <chinedubenj@gmail.com>
2023-05-27 02:58:08 -0700

    created a readme file

---
+++ README.md
@@ -0,0 +1,9 @@
+ A responsive and interactive dashboard for Upward Airlines, providing a comprehensive overview of key metrics, scorecards, documents, notifications, and system settings.
+
+This HTML code represents a dashboard interface for an airline company called Upward Airlines. The dashboard includes a navbar at the top with various menu options and icons for quick actions. It also contains a side menu for navigation and a main content section displaying charts and data columns.
+
+The code includes the necessary HTML structure, CSS stylesheets, and JavaScript files to create the interactive and visually appealing dashboard. It utilizes font icons from Font Awesome and Chart.js library for chart rendering.
+
+The dashboard offers functionality such as search, favorites, scorecards, documents, notifications, system settings, history, and more. It provides a user-friendly interface for administrators to manage and monitor various aspects of the airline's performance.
+
+Overall, the Upward Airlines Dashboard offers a sleek and intuitive interface to access and analyze crucial data and metrics related to the airline's operations, performance, and key performance indicators.
--- js/charts.js
+++ js/charts.js
@@ -1,44 +1,23 @@
-google.charts.load('current', { packages: ['corechart'] });
-google.charts.setOnLoadCallback(drawChart);
+// Get the canvas element
+var ctx = document.getElementById('myChart').getContext('2d');
 
-
-// Function to draw the chart
-function drawChart() {
-    // Create the data table
-    var data = new google.visualization.DataTable();
-    data.addColumn('string', 'Category');
-    data.addColumn('number', 'Percentage');
-    data.addRows([
-        ['Category 1', 30],
-        ['Category 2', 20],
-        ['Category 3', 50]
-    ]);
-
-    // Set chart options
-    var options = {
-        width: '100%',
-        height: '100%',
-        chartArea: {
-            left: 10,
-            top: 10,
-            right: 10,
-            bottom: 10,
-            width: '100%',
-            height: '100%'
-        },
-        legend: {
-            position: 'bottom'
-        },
-        pieSliceText: 'percentage',
-        is3D: true
-    };
-
-    // Instantiate and draw the chart, passing in the options
-    var chart = new google.visualization.PieChart(document.getElementById('myPieChart'));
-    chart.draw(data, options);
-
-    // Redraw the chart when the window is resized
-    window.addEventListener('resize', function () {
-        chart.draw(data, options);
-    });
-}
+// Create the chart
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Category 1', 'Category 2', 'Category 3'],
+    datasets: [{
+      label: 'Data',
+      data: [30, 20, 50],
+      backgroundColor: ['red', 'green', 'blue']
+    }]
+  },
+  options: {
+    responsive: true,
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
