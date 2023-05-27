5bdc645460c2d74aee1da8b2fc7d006b5fcd1bdf
Benjtalkshow <chinedubenj@gmail.com>
2023-05-27 04:11:02 -0700

    added some bar charts

--- js/charts.js
+++ js/charts.js
@@ -1,5 +1,6 @@
+
 // Get the canvas element
-var ctx = document.getElementById('myChart').getContext('2d');
+var ctx = document.getElementById('myChartJs').getContext('2d');
 
 // Create the chart
 var myChart = new Chart(ctx, {
@@ -9,7 +10,33 @@ var myChart = new Chart(ctx, {
     datasets: [{
       label: 'Data',
       data: [30, 20, 50],
-      backgroundColor: ['red', 'green', 'blue']
+      backgroundColor: ['#f2086d', '#1ab395', '#0b7eca']
+    }]
+  },
+  options: {
+    responsive: true,
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+console.log(ctx);
+
+// Get the canvas element
+var cts = document.getElementById('myChart').getContext('2d');
+
+// Create the chart
+var myCharts = new Chart(cts, {
+  type: 'bar',
+  data: {
+    labels: ['Category 1', 'Category 2', 'Category 3'],
+    datasets: [{
+      label: 'Data',
+      data: [30, 20, 50],
+      backgroundColor: ['#f2086d', '#1ab395', '#0b7eca']
     }]
   },
   options: {
